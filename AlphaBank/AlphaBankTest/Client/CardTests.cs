e1a6d2cbd594c545c9319ccd5d879c02a949798d
Vikram Kulkarni <vikram.kulkarni@nomura.com>
2019-05-20 18:23:58 +0100

    Added client tests

---
+++ AlphaBank/AlphaBankTest/Client/CardTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using AlphaBank.Client;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlphaBankTest.Client
+{
+    [TestClass]
+    public class CardTests
+    {
+        private Card _card;
+
+        [TestMethod]
+        public void ValidPinReturnsTrue()
+        {
+            //Arrange
+            _card = new Card(123123132132, "Jane Doe", DateTime.UtcNow.AddYears(2), 6789);
+
+            //Act
+            var isValidPin = _card.IsValidPin(6789);
+
+            //Assert
+            Assert.IsTrue(isValidPin);
+        }
+
+
+        [TestMethod]
+        public void InValidPinReturnsFalse()
+        {
+            //Arrange
+            _card = new Card(123123132132, "Jane Doe", DateTime.UtcNow.AddYears(2), 6789);
+
+            //Act
+            var isValidPin = _card.IsValidPin(1234);
+
+            //Assert
+            Assert.IsFalse(isValidPin);
+        }
+    }
+}
